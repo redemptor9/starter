bca0cf8cfc3c6f77ef61cb5291ab1c917c620eb3
redemptor <redemptorokongo7@gmail.com>
2020-04-23 20:04:10 +0300

    first commit

---
+++ starter/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Basic html file</title>
+    <link rel="stylesheet" href="css/bootstrap.css" >
+
+</head>
+<body>
+<div class="container">
+    <!--Row with two equal columns-->
+    <div class="row">
+        <div class="col-md-6">Column left</div>
+        <div class="col-md-6">Column right</div>
+    </div>
+    
+    <!--Row with two columns divided in 1:2 ratio-->
+    <div class="row">
+        <div class="col-md-4">Column left</div>
+        <div class="col-md-8">Column right</div>
+    </div>
+    
+    <!--Row with two columns divided in 1:3 ratio-->
+    <div class="row">
+        <div class="col-md-3">Column left</div>
+        <div class="col-md-9">Column right</div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+    <script src="js/boootstrap.js" ></script>
+   
+</body>
+</html>
+
